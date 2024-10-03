d231a1a1b7d8ac4ba4ce3e8deef03067e3c507ad
6-keem <6ukeem@gmail.com>
2024-10-03 13:57:37 +0900

    initial commit

---
+++ src/main/java/com/precapstone/fiveguys_backend/FiveguysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.precapstone.fiveguys_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveguysBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiveguysBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/precapstone/fiveguys_backend/FiveguysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precapstone.fiveguys_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveguysBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
