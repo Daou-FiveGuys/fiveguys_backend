3cb96c81d9dfb788b011f01fec3b13f48c85a3e1
6-keem <6ukeem@gmail.com>
2024-10-03 22:14:36 +0900

    add swagger

--- src/main/java/com/precapstone/fiveguys_backend/config/SwaggerConfig.java
+++ src/main/java/com/precapstone/fiveguys_backend/config/SwaggerConfig.java
@@ -2,11 +2,20 @@ package com.precapstone.fiveguys_backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("springdoc-public")
+                .pathsToMatch("/**")
+                .build();
+    }
 
     @Bean
     public OpenAPI openAPI() {
