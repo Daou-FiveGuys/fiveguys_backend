cd4806e06831ac1289f507d0ad78ebc35f5aac88
junni01kim <audwnssp@naver.com>
2024-11-06 01:55:27 +0900

    application-local.yml 오류로 인한 spring.mail.host 경로 spring.gmail로 수정

--- src/main/java/com/precapstone/fiveguys_backend/common/config/MailConfig.java
+++ src/main/java/com/precapstone/fiveguys_backend/common/config/MailConfig.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
-    @Value("${spring.mail.host.name}")
+    @Value("${spring.gmail}")
     private String host;
 
     @Value("${spring.mail.port}")
