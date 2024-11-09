761c7c1093820bcf108d623f75cf7ddb4a10cbde
6-keem <6ukeem@gmail.com>
2024-11-10 02:29:57 +0900

    [feat] : implement NLP service using 4.0 mini model

---
+++ src/main/java/com/precapstone/fiveguys_backend/api/nlp/GptController.java
@@ -0,0 +1,29 @@
+package com.precapstone.fiveguys_backend.api.nlp;
+
+import com.precapstone.fiveguys_backend.common.CommonResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "NLP", description = "이미지 프롬프팅, 문장 구체화, 주요 문장 추출")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/ai/gpt")
+public class GptController {
+    private final GptService gptService;
+    @PostMapping("/generate-text")
+    public ResponseEntity<CommonResponse> makeText(@RequestHeader("Authorization") String authorization, @RequestBody NlpParamDTO nlpParamDTO) {
+        return ResponseEntity.ok(gptService.makeText(authorization, nlpParamDTO.getText()));
+    }
+
+    @PostMapping("/generate-prompt")
+    public ResponseEntity<CommonResponse> generatePrompt(@RequestHeader("Authorization") String authorization, @RequestBody NlpParamDTO nlpParamDTO) {
+        return ResponseEntity.ok(gptService.makeText(authorization, nlpParamDTO.getText()));
+    }
+
+    @PostMapping("/extract-key-points")
+    public ResponseEntity<CommonResponse> extractKeyPoints(@RequestHeader("Authorization") String authorization, @RequestBody NlpParamDTO nlpParamDTO) {
+        return ResponseEntity.ok(gptService.makeText(authorization, nlpParamDTO.getText()));
+    }
+}
---
+++ src/main/java/com/precapstone/fiveguys_backend/api/nlp/GptService.java
@@ -0,0 +1,11 @@
+package com.precapstone.fiveguys_backend.api.nlp;
+
+import com.precapstone.fiveguys_backend.common.CommonResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GptService {
+    public CommonResponse makeText(String authorization, String text){
+        return null;
+    }
+}
